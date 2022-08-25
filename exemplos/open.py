bae9a8a8be29947d620f752a271cd33c94874ecb
Adilson_krischanski <krischanski.adilson@gmail.com>
2022-08-25 20:31:33 -0300

    include exemplos

---
+++ exemplos/open.py
@@ -0,0 +1,12 @@
+#@adilson krischanski
+import numpy as np
+import cv2
+img=cv2.imread("images/red.bmp")
+#display an image
+cv2.imshow('image',img)
+b,g,r=cv2.split(img)
+cv2.imshow('b',b)
+cv2.imshow('g',g)
+cv2.imshow('r',r)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
