28800f7e67c7f7ab96134d6332f84ac0738c209a
Adilson_krischanski <krischanski.adilson@gmail.com>
2022-10-27 16:36:32 -0300

    initial commit in trabalho4

--- trabalho03/certo.py
+++ trabalho03/certo.py
@@ -1,12 +1,13 @@
+from pickletools import uint8
+from time import process_time_ns
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import cv2 
-
 from skimage import data, io, filters, color 
 
 
-path = 'figuraEscura.jpg'
+path = './imgs/outono_LC.png'
 arquivo_saida = 'output_image.jpg'
 
 
@@ -14,14 +15,54 @@ arquivo_saida = 'output_image.jpg'
 # r,g,b=cv2.split(img_read)
 
 img = io.imread(path) # [:,:,:3]
-print(img)
-# print(img)
-img_rgb=img
 
-img_np = np.asarray(img_rgb, np.uint8)
+img_np = np.asarray(img, np.uint8)
 
-yiq =  color.rgb2yiq(img).reshape(-1, 3)
-print(yiq)
+yiq =  color.rgb2yiq(img)
 
-# io.imshow(img)
-# plt.show()
+y, i, q = cv2.split(yiq)
+
+
+img_array = np.asarray(y)
+
+arraycerto = []
+for x in img_array:
+    aux =[]
+    for j in x:
+        j=int(j*1000)
+        aux.append(j)
+    arraycerto.append(aux)
+
+arraycerto = np.asarray(arraycerto, np.uint8)
+
+histo = np.bincount(arraycerto.flatten(), minlength=256)
+
+n_pixels = np.sum(histo)
+
+histo = histo/n_pixels
+
+c_histo = np.cumsum(histo)
+
+transf = np.floor(255 * c_histo).astype(np.uint8)
+
+img_lista = list(arraycerto.flatten())
+
+#merge e  mudar
+mudado = []
+
+for x in transf:
+    j=j/1000
+    mudado.append(j)
+
+mudado = np.asarray(mudado, np.float64)
+img_saida = cv2.merge([mudado,i,q])
+rgb = color.yiq2rgb(img_saida)
+
+
+out_img_lista = [rgb[p] for p in img_lista]
+
+out_img_array = np.reshape(np.asarray(out_img_lista), arraycerto.shape)
+
+eq_img = Image.fromarray(out_img_array, mode='L')
+
+eq_img.save(arquivo_saida)
---
+++ trabalho4/code2.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread, imshow
+from skimage.color import rgb2hsv, rgb2gray, rgb2yuv
+from skimage import color, exposure, transform
+from skimage.exposure import equalize_hist
+
+class image():
+    def __init__(self,path):
+        self.img = imread(path)
+
+    
+
+    def fourier_masker_ver(self, i):
+        image = self.img
+        f_size = 15
+        dark_image_grey_fourier = np.fft.fftshift(np.fft.fft2(rgb2gray(image)))
+        dark_image_grey_fourier[:225, 235:240] = i
+        dark_image_grey_fourier[-225:,235:240] = i
+        fig, ax = plt.subplots(1,3,figsize=(15,15))
+        ax[0].imshow(np.log(abs(dark_image_grey_fourier)), cmap='gray')
+        ax[0].set_title('Masked Fourier', fontsize = f_size)
+        ax[1].imshow(rgb2gray(image), cmap = 'gray')
+        ax[1].set_title('Greyscale Image', fontsize = f_size);
+        ax[2].imshow(abs(np.fft.ifft2(dark_image_grey_fourier)), 
+                        cmap='gray')
+        ax[2].set_title('Transformed Greyscale Image', 
+                        fontsize = f_size);
+
+
+    def fourier_masker_hor(self, i):
+        image = self.img
+        f_size = 15
+        dark_image_grey_fourier = np.fft.fftshift(np.fft.fft2(rgb2gray(image)))
+        dark_image_grey_fourier[235:240, :230] = i
+        dark_image_grey_fourier[235:240,-230:] = i
+        fig, ax = plt.subplots(1,3,figsize=(15,15))
+        ax[0].imshow(np.log(abs(dark_image_grey_fourier)), cmap='gray')
+        ax[0].set_title('Masked Fourier', fontsize = f_size)
+        ax[1].imshow(rgb2gray(image), cmap = 'gray')
+        ax[1].set_title('Greyscale Image', fontsize = f_size);
+        ax[2].imshow(abs(np.fft.ifft2(dark_image_grey_fourier)), 
+                        cmap='gray')
+        ax[2].set_title('Transformed Greyscale Image', 
+                        fontsize = f_size);
+        
+    def fourier_iterator(self, value_list):
+        image = self.img
+        for i in value_list:
+            self.fourier_masker_ver(i)
+
+    def fourier_transform_rgb(self):
+        image = self.img
+        f_size = 25
+        transformed_channels = []
+        for i in range(3):
+            rgb_fft = np.fft.fftshift(np.fft.fft2((image[:, :, i])))
+            rgb_fft[:225, 235:237] = 1
+            rgb_fft[-225:,235:237] = 1
+            transformed_channels.append(abs(np.fft.ifft2(rgb_fft)))
+        
+        final_image = np.dstack([transformed_channels[0].astype(int), 
+                                transformed_channels[1].astype(int), 
+                                transformed_channels[2].astype(int)])
+    
+        fig, ax = plt.subplots(1, 2, figsize=(17,12))
+        ax[0].imshow(image)
+        ax[0].set_title('Original Image', fontsize = f_size)
+        ax[0].set_axis_off()
+        
+# fourier_masker(dark_image, 1)
+
+
+if __name__== "__main__":
+
+    img = image('folhas1_Reticulada.jpg')
+    img.fourier_transform_rgb()
